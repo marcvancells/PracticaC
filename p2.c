a35d4a5eb7bfb45c506a03aaa3e4f28a546f9913
Marc Vancells <vancellsmarc@gmail.com>
2023-10-23 09:30:58 +0200

    Versió Constants declarades

---
+++ p2.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#define N 512
+float Mat[N][N], MatDD[N][N], V1[N], V2[N], V3[N], V4[N];
+
+void InitData(){
+int i,j;
+srand(8824553);
+for( i = 0; i < N; i++ )
+ for( j = 0; j < N; j++ ){
+ Mat[i][j]=(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX)));
+ if ( (abs(i - j) <= 3) && (i != j))
+ MatDD[i][j] = (((i*j)%3) ? -1 : 1)*(rand()/(1.0*RAND_MAX));
+ else if ( i == j )
+ MatDD[i][j]=(((i*j)%3)?-1:1)*(10000.0*(rand()/(1.0*RAND_MAX)));
+ else MatDD[i][j] = 0.0;
+ }
+for( i = 0; i < N; i++ ){
+ V1[i]=(i<N/2)?(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX))):0.0;
+ V2[i]=(i>=N/2)?(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX))):0.0;
+ V3[i]=(((i*j)%5)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX)));
+}
+}
+
+int main(){
+return 0;
+}
+
