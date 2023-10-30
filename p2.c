0632cf05873a50ec3fa02cafa381f7d4393864cc
Marc Vancells <vancellsmarc@gmail.com>
2023-10-30 08:53:32 +0100

    Ex 1

--- p2.c
+++ p2.c
@@ -3,11 +3,16 @@
 #include <math.h>
 
 #define N 512
-float Mat[N][N], MatDD[N][N], V1[N], V2[N], V3[N], V4[N];
+float Mat[N][N]; 
+float MatDD[N][N]; 
+float V1[N]; 
+float V2[N]; 
+float V3[N]; 
+float V4[N];
 
 void InitData(){
 int i,j;
-srand(8824553);
+srand(4422543);
 for( i = 0; i < N; i++ )
  for( j = 0; j < N; j++ ){
  Mat[i][j]=(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX)));
@@ -24,7 +29,40 @@ for( i = 0; i < N; i++ ){
 }
 }
 
-int main(){
-return 0;
+void PrintVect( float vect[N], int from, int numel ){
+        int i;
+        for(i=from; i < from + numel; i++){
+                printf("%f ", vect[i]);
+
+        }
 }
 
+void PrintRow( float mat[N][N], int row, int from, int numel ){
+	int i;
+	for(i=from; i < from+numel; i++){
+		printf("%f",mat[row][i]);
+
+	}
+}
+
+int main(int argc, char** argv){
+        InitData();
+        int numel,from,row;
+	printf("Nombre d'elements: ");
+        scanf("%d", &numel);
+	printf("Posició Vector d'inici: ");
+        scanf("%d", &from);
+	printf("Quina fila: ");
+	scanf("%d", &row);
+        int vect;
+	printf("Quin vecotor? ");
+        scanf("%d", &vect);
+
+	//float* vector[3] = {V1, V2, V3};
+
+	PrintVect(V1, from, numel );
+	
+	//PrintRow( float mat[row][i], int row, int from, int numel);
+		
+	return 0;
+}
